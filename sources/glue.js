7331c0259492b32206ac8f6c3745ba87f33054df
David Polak <git@davidpolak.cz>
2018-05-01 19:46:02 +0200

    Glue.js added xml parsing and cwrapped functions

--- sources/glue.js
+++ sources/glue.js
@@ -7,6 +7,18 @@
  */
 
 /**
+ * Converts TypedArray to Uint8Array
+ */
+Module['heapArray'] = function (arr) {
+  var numBytes = arr.length * arr.BYTES_PER_ELEMENT
+  var ptr = this._malloc(numBytes)
+  // heapBytes.byteOffset is the ptr
+  var heapBytes = new Uint8Array(this.HEAPU8.buffer, ptr, numBytes)
+  heapBytes.set(new Uint8Array(arr.buffer))
+  return heapBytes
+}
+
+/**
  * Sends a XHR request to url Module.modelDescriptionFile,
  * expecting to find a FMU 2.0 modelDescription.xml file.
  * @return {Promise} rejects on not found
@@ -39,13 +51,126 @@ Module['loadXML'] = function () {
 }
 
 /**
-* There is a bug in emscripten which causes Module.then to enter an
-* infinite loop when called from Promise.all(), this is a workaround.
-* Provides Module.ready Promise which is properly thennable.
-*
-* see: https://github.com/kripken/emscripten/issues/5820
-* TODO: revisit when 5820 is resolved
-*/
+ * Parses FMU modelDescription.xml.
+ */
+Module['parseXML'] = function () {
+  var resolver = this.xmlDoc.createNSResolver(
+    this.xmlDoc.ownerDocument == null
+      ? this.xmlDoc.documentElement
+      : this.xmlDoc.ownerDocument.documentElement
+  )
+
+  // GUID is used for model identification
+  this.guid = this.xmlDoc.evaluate(
+    'string(//fmiModelDescription/@guid)',
+    this.xmlDoc, resolver, XPathResult.STRING_TYPE, null).stringValue
+
+  // identifier is prepended to names of FMI C functions
+  this.identifier = this.xmlDoc.evaluate(
+    'string(//fmiModelDescription/CoSimulation/@modelIdentifier)',
+    this.xmlDoc, resolver, XPathResult.STRING_TYPE, null).stringValue
+
+  // TODO: parse variables
+  // TODO: parse parameters
+}
+
+/**
+ * Cwraps all of the necessary C functions to be used by the module.
+ */
+Module['wrapFunctions'] = function () {
+  this.snprintf = this.cwrap(
+    'snprintf', 'number', [
+      'number', 'number', 'number', 'number'
+    ]
+  )
+
+  this.fmi2GetTypesPlatform = this.cwrap(
+    this.identifier + '_fmi2GetTypesPlatform',
+    'string'
+  )
+
+  this.fmi2GetVersion = this.cwrap(
+    this.identifier + '_fmi2GetVersion',
+    'string'
+  )
+
+  this.createFmi2CallbackFunctions = this.cwrap(
+    'createFmi2CallbackFunctions', 'number', [
+      'number'
+    ]
+  )
+
+  this.fmi2Instantiate = this.cwrap(
+    this.identifier + '_fmi2Instantiate', 'number', [
+      'string',
+      'number',
+      'string',
+      'string',
+      'number',
+      'number',
+      'number'
+    ]
+  )
+
+  this.fmi2SetupExperiment = this.cwrap(
+    this.identifier + '_fmi2SetupExperiment', 'number', [
+      'number',
+      'number',
+      'number',
+      'number',
+      'number',
+      'number'
+    ]
+  )
+
+  this.fmi2EnterInitializationMode = this.cwrap(
+    this.identifier + '_fmi2EnterInitializationMode', 'number', [
+      'number'
+    ]
+  )
+
+  this.fmi2ExitInitializationMode = this.cwrap(
+    this.identifier + '_fmi2ExitInitializationMode', 'number', [
+      'number'
+    ]
+  )
+
+  this.fmi2GetReal = this.cwrap(
+    this.identifier + '_fmi2GetReal', 'number', [
+      'number',
+      'number',
+      'number',
+      'number'
+    ]
+  )
+
+  this.fmi2SetReal = this.cwrap(
+    this.identifier + '_fmi2SetReal', 'number', [
+      'number',
+      'number',
+      'number',
+      'number'
+    ]
+  )
+
+  this.fmi2DoStep = this.cwrap(
+    this.identifier + '_fmi2DoStep', 'number', [
+      'number',
+      'number',
+      'number',
+      'number'
+    ]
+  )
+}
+
+/**
+ * There is a bug in emscripten which causes Module.then to enter an
+ * infinite loop when called from Promise.all(), this is a workaround.
+ * Provides Module.ready Promise which is properly thennable.
+ *
+ * see: https://github.com/kripken/emscripten/issues/5820
+ * TODO: revisit when 5820 is resolved
+ */
 Module['ready'] = new Promise(function (resolve, reject) {
   delete Module['then']
   Module['onAbort'] = function (what) {
@@ -60,6 +185,8 @@ Module['loadFmiFunctions'] = function () {
   var self = this
   return new Promise(function (resolve, reject) {
     self.loadXML().then(function (val) {
+      self.parseXML()
+      self.wrapFunctions()
       resolve(self)
     }).catch(function (err) {
       reject(err)
