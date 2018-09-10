901afc7e33f0f6f713c49f2e809228de638a83f1
David Polak <git@davidpolak.cz>
2018-09-10 18:20:35 +0200

    Fix run.sh to actually run the code

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ sudo docker run -d \
   --name bodylight.js.fmu.compiler \
   --mount type=bind,source="$(pwd)"/input,target=/input \
   --mount type=bind,source="$(pwd)"/output,target=/output \
-  bodylight.js.fmu.compiler:latest
+  bodylight.js.fmu.compiler:latest bash worker.sh
