9cab1275fdb4e66fe300d635a703be72b8186de7
David Polak <git@davidpolak.cz>
2019-02-27 15:24:52 +0100

    Fix .gitignore

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 sudo docker build -t bodylight.js.fmu.compiler "$(pwd)"
 
-docker run -d \
+sudo docker run -d \
   --name bodylight.js.fmu.compiler \
   --mount type=bind,source="$(pwd)"/input,target=/input \
   --mount type=bind,source="$(pwd)"/output,target=/output \
