82092ba2dc00363299b02b2339a59fd6aa995c85
Tomas Kulhanek <tomaton@centrum.cz>
2020-07-09 16:06:34 +0100

    UPDATE

--- compile.sh
+++ compile.sh
@@ -2,4 +2,4 @@
 # output will be written to /output directory
 # input file will be removed
 # docker earlier verion 1.13.1
-sudo docker run -d   --name bodylight.js.fmu.compiler -v $(pwd):/input -v $(pwd)/output:/output --rm bodylight.js.fmu.compiler:latest bash worker.sh $1
+sudo docker run -d   --name bodylight.js.fmu.compiler -v $(pwd)/input:/input -v $(pwd)/output:/output --rm bodylight.js.fmu.compiler:latest bash worker.sh $1
